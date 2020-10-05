d2d77352a7562e51d069ecc9702e85a86682d0c5
Ben <bcharumbira@lbpro.pl>
2020-10-05 16:04:17 +0200

    Added Wave Ani

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import PageHeader from '../widgets/PageHeader';
+import Wave from '../widgets/Wave';
 
 export default function Home() {
 
   
   return (
-    <>
+    <div style={{overflowY: "hidden"}}>
       <PageHeader />
-    </>
+      <Wave />
+    </div>
   )
 }
---
+++ widgets/Wave/index.js
@@ -0,0 +1,13 @@
+import styles from './style.module.scss'
+
+export default function Wave() {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.ocean}>
+                <div className={styles.wave}></div>
+                <div className={styles.wave}></div>
+            </div>
+        </div>
+    )
+}
