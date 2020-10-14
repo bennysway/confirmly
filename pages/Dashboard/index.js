a44bf7b453ff37d5ffc73c0e7dfed3b135b33944
Ben <bcharumbira@lbpro.pl>
2020-10-14 16:11:23 +0200

    Added Submit Page Sample

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -4,13 +4,20 @@ import useScript from '../../hooks/useScript'
 
 export default function Dashboard() {
 
+    /* Jquery JS*/
     useScript("https://code.jquery.com/jquery-3.5.1.slim.min.js")
+    /* Bootstrap JS*/
     useScript("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js")
     useScript("https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js")
+    useScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/0.9.0/bootstrap-progressbar.min.js")
+    /* Vendor JS */
     useScript("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js")
     useScript("https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js")
     useScript("https://cdnjs.cloudflare.com/ajax/libs/animsition/4.0.2/js/animsition.min.js")
-    useScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/0.9.0/bootstrap-progressbar.min.js")
+    useScript("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js")
+    useScript("https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js")
+    useScript("https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js")
+    useScript("https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js")
 
     return (
         <>
@@ -83,11 +90,11 @@ export default function Dashboard() {
                                     </li>
                                     <li>
                                         <a href="table.html">
-                                            <i className="fas fa-table" />Tables</a>
+                                            <i className="fas fa-table" />Records</a>
                                     </li>
                                     <li>
-                                        <a href="form.html">
-                                            <i className="far fa-check-square" />Forms</a>
+                                        <a href="/Dashboard/Submit">
+                                            <i className="far fa-check-square" />Submit</a>
                                     </li>
                                     <li>
                                         <a href="calendar.html">
@@ -193,8 +200,8 @@ export default function Dashboard() {
                                             <i className="fas fa-table" />Records</a>
                                     </li>
                                     <li>
-                                        <a href="form.html">
-                                            <i className="far fa-check-square" />Forms</a>
+                                        <a href="/Dashboard/Submit">
+                                            <i className="far fa-check-square" />Submit</a>
                                     </li>
                                     <li className="has-sub">
                                         <a className="js-arrow" href="#">
